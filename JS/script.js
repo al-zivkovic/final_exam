2c59c2ef142a093e07ad0941f0abeae04231700f
Aleksandar <aldaman2@outlook.com>
2021-12-10 13:28:38 -0800

    final commit/final touches

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,25 @@
 function darkMode() {
     var body = document.body;
-    body.classList.toggle("dark-theme")
+    body.classList.toggle("dark-theme");
 };
 
+let switchModes = document.querySelector(".toggle");
 
-let switchModes = document.querySelector(".toggle")
-
-document.addEventListener("click", () => {
+switchModes.addEventListener("click", () => {
     darkMode()
 
 });
 
-document.querySelector('#frm-btn').addEventListener('click', function(event) {
+document.querySelector('button').addEventListener('click', function(event) {
     let fields = {
         email: "",
         name: "",
         message: "",
-    }
+    };
 
-    fields.email = document.getElementsByName('email')[0].value;
-    fields.name = document.getElementsByName('name')[0].value;
-    fields.message = document.getElementsByName('email')[0].value;
+    fields.email = document.querySelector('#email')[0].value;
+    fields.name = document.querySelector('#name')[0].value;
+    fields.message = document.getElementsByTagName('textarea')[0].value;
 
     console.log(fields)
     event.preventDefault()
